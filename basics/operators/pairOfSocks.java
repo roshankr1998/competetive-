620f1df9cedd66275a0ed20d849465adff285711
roshankr1998 <roshan.kumar.rk109@gmail.com>
2022-03-08 01:46:35 +0530

    all

--- basics/operators/pairOfSocks.java
+++ basics/operators/pairOfSocks.java
@@ -37,6 +37,7 @@ public class pairOfSocks {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        
         System.out.println(n+1);
         sc.close();
     }
