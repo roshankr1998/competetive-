1355be1ea06ac1f3561c64e0e0dbc0c541fe706d
roshankr1998 <roshan.kumar.rk109@gmail.com>
2022-03-14 21:35:31 +0530

    commit

---
+++ basics/arraylist/queries.java
@@ -0,0 +1,25 @@
+package basics.arraylist;
+
+import java.util.Scanner;
+
+public class queries {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int maxsum[]=new int[n];
+        maxsum[0]=a[0];
+        for(int i=1;i<n;i++){
+            maxsum[i]=maxsum[i-1]+a[i];
+        }
+
+        int l=sc.nextInt();
+        int r=sc.nextInt();
+        if(l==0) System.out.println(maxsum[r]);
+        else System.out.println(maxsum[r]-maxsum[l-1]);
+    }
+    
+}
---
+++ basics/arraylist/submatricessum.java
@@ -0,0 +1,17 @@
+package basics.arraylist;
+import java.util.*;
+public class submatricessum {
+  public static void main(String[] args) {
+    int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+    long sum=0;
+    int n=3; int m=a[0].length;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            sum+=(i+1)*(j+1)*a[i][j]*(n-i)*(m-j);
+        }
+    }
+    System.out.println(sum);
+  }  
+    
+    
+}
